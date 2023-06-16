aae4fe8116abd9bb57b0659123f09c6dfaf439f7
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2023-06-17 06:35:09 +0700

    build(main): init project

---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "blue-opaque": "rgb(13 42 148 / 18%)",
+        gray: {
+          0: "#fff",
+          100: "#fafafa",
+          200: "#eaeaea",
+          300: "#999999",
+          400: "#888888",
+          500: "#666666",
+          600: "#444444",
+          700: "#333333",
+          800: "#222222",
+          900: "#111010",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.700"),
+            a: {
+              color: theme("colors.blue.500"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+              },
+              code: { color: theme("colors.blue.400") },
+            },
+            "h2,h3,h4": {
+              "scroll-margin-top": spacing[32],
+            },
+            thead: {
+              borderBottomColor: theme("colors.gray.200"),
+            },
+            code: { color: theme("colors.pink.500") },
+            "blockquote p:first-of-type::before": false,
+            "blockquote p:last-of-type::after": false,
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    typography: ["dark"],
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
